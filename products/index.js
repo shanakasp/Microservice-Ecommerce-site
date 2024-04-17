787218dfe28626294191103d55075149e738691b
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-04-17 18:33:59 +0530

    added index.js and index.js

---
+++ products/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/", (req, res, next) => {
+  return res.status(201).json({ msg: "Product" });
+});
+
+app.listen(8002, () => {
+  console.log("Product is listening on part 8001");
+});
